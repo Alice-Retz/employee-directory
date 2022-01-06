a3540291d806c0ed385be75886cf0a959f792b04
Alice-Retz <alicegretz@gmail.com>
2022-01-06 14:48:15 -0600

    Mobbed with Triana, Simon, and Aemilius

---
+++ src/views/Home/Home.jsx
@@ -0,0 +1,15 @@
+import { Link, Redirect } from 'react-router-dom';
+import { useUser } from '../../context/UserContext';
+
+export default function Home() {
+  const auth = useUser();
+  if (auth.user.email) return <Redirect to="/profile" />;
+  return (
+    <section>
+      <h1>Welcome to the Acme Directory</h1>
+      <p>
+        Please <Link to="/login">login</Link> to use the directory
+      </p>
+    </section>
+  );
+}
